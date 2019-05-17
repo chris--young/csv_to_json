161386ed736ec0e163df1383190574fac60eab70
Chris Young <young.c.5690@gmail.com>
2019-05-17 11:35:17 -0700

    Don't use global JSON

--- main.d.ts
+++ main.d.ts
@@ -1,11 +1,11 @@
 declare namespace csv_to_json {
-  export type JSON = { [key: string]: string };
-  export type Callback = (error: Error, result: JSON) => void;
+  export type Json = { [key: string]: string };
+  export type Callback = (error: Error, result: Json) => void;
 
-  export function from_file(filename: string, cb: Callback): Promise<JSON[]>|undefined;
+  export function from_file(filename: string, cb: Callback): Promise<Json[]>|undefined;
 }
 
-declare function csv_to_json(csv: string): JSON[];
+declare function csv_to_json(csv: string): csv_to_json.Json[];
 
 export = csv_to_json;
 
